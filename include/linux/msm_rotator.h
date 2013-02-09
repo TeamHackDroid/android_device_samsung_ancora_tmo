a80b33f2e9e86dc5041138d1cc283a3b35fcdc68
Doadin <tbkizle@gmail.com>
2013-02-09 11:59:01 -0500

    Update

--- include/linux/msm_rotator.h
+++ include/linux/msm_rotator.h
@@ -31,7 +31,6 @@ struct msm_rotator_img_info {
 	unsigned char   rotations;
 	int enable;
 	unsigned int	downscale_ratio;
-	unsigned int secure;
 };
 
 struct msm_rotator_data_info {
@@ -59,3 +58,4 @@ struct msm_rotator_platform_data {
 #endif
 };
 #endif
+
