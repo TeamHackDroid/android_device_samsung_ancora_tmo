2de2240392877c9a0e10a5ce39604e85cc7b3242
doadin <tbkizle@gmail.com>
2013-02-09 11:21:52 -0500

    BackDate To ICS

--- camerahal/Android.mk
+++ camerahal/Android.mk
@@ -1,6 +1,3 @@
-$(shell mkdir -p $(OUT)/obj/SHARED_LIBRARIES/libseccameraadaptor_intermediates/)
-$(shell touch $(OUT)/obj/SHARED_LIBRARIES/libseccameraadaptor_intermediates/export_includes)
-
 LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
@@ -11,7 +8,7 @@ LOCAL_MODULE := camera.$(TARGET_BOARD_PLATFORM)
 LOCAL_MODULE_TAGS := optional
 
 LOCAL_SRC_FILES := cameraHAL.cpp
-LOCAL_C_INCLUDES := $(TOP)/frameworks/native/include
+LOCAL_C_INCLUDES := $(TOP)/frameworks/base/include
 
 LOCAL_SHARED_LIBRARIES := liblog libutils libcutils
 LOCAL_SHARED_LIBRARIES += libui libhardware libcamera_client
