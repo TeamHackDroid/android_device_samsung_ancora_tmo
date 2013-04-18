c2bfdb53f237e0607ef3e8bad5d6312eae272d79
doadin <tbkizle@gmail.com>
2013-04-18 02:44:12 -0300

    Update ancora_tmo.mk

--- ancora_tmo.mk
+++ ancora_tmo.mk
@@ -115,7 +115,9 @@ PRODUCT_PACKAGES += \
     audio.primary.msm7x30 \
     audio_policy.msm7x30 \
     audio.a2dp.default \
-    audio.usb.default 
+    audio.usb.default \
+    libaudio-resampler \
+    libaudioparameter
 
 PRODUCT_PACKAGES += \
     libmm-omxcore \
@@ -137,7 +139,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     com.android.future.usb.accessory \
-    librpc
+    libnetcmdiface
 
 # For userdebug builds
 ADDITIONAL_DEFAULT_PROPERTIES += \
