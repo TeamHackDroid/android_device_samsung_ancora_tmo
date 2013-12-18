5e6a366a58abad684d0fbb69cfb1687f5bff218c
Doadin <tbkizle@gmail.com>
2013-12-18 17:43:35 -0500

    Update Cleanup Reorganize

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -23,46 +23,44 @@
 # WARNING: This line must come *before* including the proprietary
 # variant, so that it gets overwritten by the parent (which goes
 # against the traditional rules of inheritance).
-USE_CAMERA_STUB := true
 
+# Vendor stuff
 # inherit from the proprietary version
 -include vendor/samsung/ancora_tmo/BoardConfigVendor.mk
-
-#TARGET_SPECIFIC_HEADER_PATH := device/samsung/ancora_tmo/include
 
 # create the folder /usr to prevent the build from failing
 $(shell mkdir -p $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/)
 
+# Platform
+TARGET_BOOTLOADER_BOARD_NAME := ancora_tmo
+TARGET_OTA_ASSERT_DEVICE := ancora_tmo,SGH-T679,ancora
 TARGET_BOARD_PLATFORM := msm7x30
 TARGET_BOARD_PLATFORM_GPU := qcom-adreno200
+TARGET_NO_BOOTLOADER := true
+TARGET_NO_RADIOIMAGE := true
+TARGET_NO_INITLOGO := true
 
-TARGET_CPU_ABI := armeabi-v7a
-TARGET_CPU_ABI2 := armeabi
-
-TARGET_ARCH_LOWMEM := true
-
-# Enable NEON feature
+# Architecture
 TARGET_ARCH := arm
 TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_ARCH_VARIANT_CPU := cortex-a8
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
 TARGET_USE_SCORPION_BIONIC_OPTIMIZATION := true
+TARGET_ARCH_LOWMEM := true
 
-COMMON_GLOBAL_CFLAGS += -DQCOM_HARDWARE 
-COMMON_GLOBAL_CFLAGS += -DBINDER_COMPAT
-COMMON_GLOBAL_CFLAGS += -DSAMSUNG_CAMERA_LEGACY
-
-TARGET_BOOTLOADER_BOARD_NAME := ancora_tmo
-TARGET_OTA_ASSERT_DEVICE := ancora_tmo,SGH-T679,ancora
-
-TARGET_NO_BOOTLOADER := true
-TARGET_NO_RADIOIMAGE := true
-
-TARGET_NO_INITLOGO := true
-
+# Screens dimension
 TARGET_SCREEN_HEIGHT := 800
 TARGET_SCREEN_WIDTH := 480
 
-# Wifi related defines
+# Kernel
+BOARD_KERNEL_BASE := 0x00400000
+BOARD_KERNEL_PAGESIZE := 4096
+TARGET_KERNEL_SOURCE := kernel/samsung/ancora_tmo
+TARGET_KERNEL_CONFIG := ancora_tmo_defconfig
+
+# WiFi
+BOARD_HAVE_SAMSUNG_WIFI          := true
 WIFI_BAND                        := 802_11_ABG
 WPA_SUPPLICANT_VERSION           := VER_0_8_X
 BOARD_WPA_SUPPLICANT_DRIVER      := NL80211
@@ -70,8 +68,6 @@ BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_bcmdhd
 BOARD_HOSTAPD_DRIVER             := NL80211
 BOARD_HOSTAPD_PRIVATE_LIB        := lib_driver_cmd_bcmdhd
 BOARD_WLAN_DEVICE                := bcmdhd
-BOARD_HAVE_SAMSUNG_WIFI          := true
-
 WIFI_DRIVER_MODULE_PATH          := "/system/lib/modules/dhd.ko"
 WIFI_DRIVER_MODULE_NAME          := "dhd"
 WIFI_DRIVER_MODULE_ARG           := "firmware_path=/vendor/firmware/fw_bcmdhd.bin nvram_path=/vendor/firmware/nvram_net.txt"
@@ -80,26 +76,24 @@ WIFI_DRIVER_FW_PATH_PARAM        := "/sys/module/dhd/parameters/firmware_path"
 WIFI_DRIVER_FW_PATH_STA          := "/vendor/firmware/fw_bcmdhd.bin"
 WIFI_DRIVER_FW_PATH_AP           := "/vendor/firmware/fw_bcmdhd_apsta.bin"
 
-BOARD_KERNEL_BASE := 0x00400000
-BOARD_KERNEL_PAGESIZE := 4096
-
 # Bluetooth
 BOARD_HAVE_BLUETOOTH := true
 BOARD_HAVE_BLUETOOTH_BCM := true
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/ancora_tmo/bluetooth
 BOARD_BLUEDROID_VENDOR_CONF := device/samsung/ancora_tmo/bluetooth/vnd_ancora.txt
 
+# RIL
 BOARD_MOBILEDATA_INTERFACE_NAME = "pdp0"
-
-BOARD_RIL_CLASS := ../../../device/samsung/ancora_tmo/ril/
-BOARD_USES_LEGACY_RIL := true
 BOARD_USES_LIBSECRIL_STUB := true
+BOARD_USES_LEGACY_RIL := true
+BOARD_RIL_CLASS := ../../../device/samsung/ancora_tmo/ril/
 
-# Audio
-BOARD_HAVE_SAMSUNG_AUDIO := true
-BOARD_USES_QCOM_AUDIO_RESETALL := true
-BOARD_USES_QCOM_AUDIO_VOIPMUTE := true
+# GPS
+BOARD_USES_QCOM_GPS := true
+BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := msm7x30
+BOARD_VENDOR_QCOM_GPS_LOC_API_AMSS_VERSION := 50000
 
+# Graphics
 COMMON_GLOBAL_CFLAGS += -DNEW_ION_API
 BOARD_EGL_CFG := device/samsung/ancora_tmo/config/egl.cfg
 BOARD_ALLOW_EGL_HIBERNATION := true
@@ -107,47 +101,52 @@ TARGET_NO_HW_VSYNC := false
 TARGET_USES_C2D_COMPOSITION := true
 USE_OPENGL_RENDERER := true
 
-# QCOM webkit
-ENABLE_WEBGL := true
-TARGET_FORCE_CPU_UPLOAD := true
-
-TARGET_QCOM_MEDIA_VARIANT := caf
-TARGET_QCOM_DISPLAY_VARIANT := caf
-TARGET_QCOM_AUDIO_VARIANT := caf
+# Memory allocation
 TARGET_USES_ION := true
-TARGET_USES_C2D_COMPOSITION := true
-
-BOARD_USES_QCOM_HARDWARE := true
-BOARD_USES_QCOM_GPS := true
-
-# Camera
-BOARD_USES_LEGACY_OVERLAY := true
-BOARD_USES_QCOM_LEGACY_CAM_PARAMS := true
-TARGET_DISABLE_ARM_PIE := true
 BOARD_NEEDS_MEMORYHEAPPMEM := true
 
-# Lights HAL
-TARGET_PROVIDES_LIBLIGHT := true
+# QCOM
+COMMON_GLOBAL_CFLAGS += -DQCOM_HARDWARE 
+COMMON_GLOBAL_CFLAGS += -DBINDER_COMPAT
+BOARD_USES_QCOM_HARDWARE := true
+BOARD_VENDOR_QCOM_AMSS_VERSION := 6225
+TARGET_QCOM_DISPLAY_VARIANT := caf
+TARGET_QCOM_MEDIA_VARIANT := caf
+TARGET_QCOM_AUDIO_VARIANT := caf
 
 # Power HAL
 TARGET_PROVIDES_POWERHAL := true
 
-BOARD_VENDOR_QCOM_AMSS_VERSION := 6225
-BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := msm7x30
-BOARD_VENDOR_QCOM_GPS_LOC_API_AMSS_VERSION := 50000
+# Lights HAL
+TARGET_PROVIDES_LIBLIGHT := true
+
+# Camera
+COMMON_GLOBAL_CFLAGS += -DSAMSUNG_CAMERA_LEGACY
+USE_CAMERA_STUB := true
+BOARD_USES_QCOM_LEGACY_CAM_PARAMS := true
+BOARD_USES_LEGACY_OVERLAY := true
+TARGET_DISABLE_ARM_PIE := true
+
+# Audio
+BOARD_HAVE_SAMSUNG_AUDIO := true
+BOARD_USES_QCOM_AUDIO_RESETALL := true
+BOARD_USES_QCOM_AUDIO_VOIPMUTE := true
+
+# WEBGL in WebKit
+TARGET_FORCE_CPU_UPLOAD := true
+ENABLE_WEBGL := true
 
 # Sensors
 BOARD_USE_LEGACY_SENSORS_FUSION := false
 
-TARGET_USERIMAGES_USE_EXT4 := true
+# USB mass storage
+TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/msm_hsusb/gadget/lun%d/file
+# Enable below line if compiling for a recovery version before 6.0.1.2
+#BOARD_UMS_LUNFILE := /sys/devices/platform/msm_hsusb/gadget/lun%d/file
+# End recovery stuff
 
-BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
-BOARD_VOLD_MAX_PARTITIONS := 28
-
-
-# Begin recovery stuff
-# Partition sizes must match your phone, or all hell will break loose!
-# For the Galaxy W, these are calculated from /proc/partitions
+# Partitions
+# For the Exhibit II, these are calculated from /proc/partitions
 BOARD_BOOTIMAGE_PARTITION_SIZE := 5767168
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 7864320
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 560988160
@@ -155,21 +154,39 @@ BOARD_USERDATAIMAGE_PARTITION_SIZE := 1163919360
 BOARD_CACHEIMAGE_PARTITION_SIZE := 62914560
 BOARD_FLASH_BLOCK_SIZE := 4096
 
-BOARD_USES_MMCUTILS := true
-BOARD_HAS_NO_MISC_PARTITION := true
-BOARD_HAS_SDCARD_INTERNAL := true
-TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
-BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../device/samsung/ancora_tmo/recovery/recovery_keys.c
-BOARD_CUSTOM_GRAPHICS := ../../../device/samsung/ancora_tmo/recovery/graphics.c
+# Target filesystem
+TARGET_USERIMAGES_USE_EXT4 := true
+
+# Vold stuff
+BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
+BOARD_VOLD_MAX_PARTITIONS := 28
+
+# Custom recovery files
 TARGET_RECOVERY_INITRC := device/samsung/ancora_tmo/config/init.recovery.rc
 TARGET_RECOVERY_FSTAB := device/samsung/ancora_tmo/recovery.fstab
-# Enable below line if compiling for a recovery version before 6.0.1.2
-# BOARD_UMS_LUNFILE := /sys/devices/platform/msm_hsusb/gadget/lun%d/file
-# End recovery stuff
+BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../device/samsung/ancora_tmo/recovery/recovery_keys.c
+BOARD_CUSTOM_GRAPHICS := ../../../device/samsung/ancora_tmo/recovery/graphics.c
 
-TARGET_KERNEL_CONFIG := ancora_tmo_defconfig
-TARGET_KERNEL_SOURCE := kernel/samsung/ancora_tmo
-#TARGET_PREBUILT_KERNEL := device/samsung/ancora_tmo/prebuilt/zImage
-TARGET_PREBUILT_RECOVERY_KERNEL := device/samsung/ancora_tmo/recovery/zImage
+# TWRP recovery stuff
+BOARD_HAS_SDCARD_INTERNAL := true
+BOARD_USES_MMCUTILS := true
+BOARD_HAS_NO_MISC_PARTITION := true
+BOARD_HAS_NO_SELECT_BUTTON := true
+DEVICE_RESOLUTION := 480x800
+#TW_BRIGHTNESS_PATH := /sys/devices/platform/msm_fb.196609/leds/lcd-backlight/brightness #May need to change.
+TW_MAX_BRIGHTNESS := 255
+TW_CUSTOM_POWER_BUTTON := 107
+BOARD_USE_CUSTOM_RECOVERY_FONT:= \"roboto_10x18.h\"
+TW_INTERNAL_STORAGE_PATH := "/sdcard"
+TW_INTERNAL_STORAGE_MOUNT_POINT := "sdcard"
+TW_EXTERNAL_STORAGE_PATH := "/external_sd"
+TW_EXTERNAL_STORAGE_MOUNT_POINT := "external_sd"
+TW_NO_REBOOT_BOOTLOADER := true
+TW_HAS_DOWNLOAD_MODE := true
+TW_USE_MODEL_HARDWARE_ID_FOR_DEVICE_ID := true
+TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
 
-TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/msm_hsusb/gadget/lun%d/file
+# Bootanimation
+TARGET_BOOTANIMATION_PRELOAD := true
+TARGET_BOOTANIMATION_TEXTURE_CACHE := true
+TARGET_BOOTANIMATION_USE_RGB565 := true
