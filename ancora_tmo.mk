1c3e7f3360662800c866b528a3cc473978e8e33f
Doadin <tbkizle@gmail.com>
2013-12-31 00:24:17 -0500

    Cleanup

--- ancora_tmo.mk
+++ ancora_tmo.mk
@@ -49,8 +49,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/config/fstab.qcom:root/fstab.qcom \
     $(LOCAL_PATH)/config/fstab.qcom:recovery/root/fstab.qcom \
     $(LOCAL_PATH)/config/nvram_net.txt:system/vendor/firmware/nvram_net.txt \
-    $(LOCAL_PATH)/prebuilt/SamsungServiceMode.apk:system/app/SamsungServiceMode.apk \
-    $(LOCAL_PATH)/prebuilt/TaskManager.apk:system/app/TaskManager.apk \
     $(LOCAL_PATH)/prebuilt/get_macaddrs:system/bin/get_macaddrs 
 
 # Needed to reset bootmode when leaving recovery
