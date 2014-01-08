19e84a6428bd82223f62444f3627eeab8eccfda9
Doadin <tbkizle@gmail.com>
2014-01-08 08:52:51 -0500

    update

--- camerahal/cameraHAL.cpp
+++ camerahal/cameraHAL.cpp
@@ -408,6 +408,7 @@ void CameraHAL_FixupParams(android::CameraParameters &camParams, priv_camera_dev
     camParams.set(CameraParameters::KEY_FLASH_MODE, "off");
 
     if (dev->cameraid == CAMERA_ID_FRONT) {
+	    camParams.set(CameraParameters::KEY_SUPPORTED_EFFECTS, "");
         camParams.set(CameraParameters::KEY_SUPPORTED_FLASH_MODES, "");
         camParams.set(CameraParameters::KEY_SUPPORTED_ISO_MODES, "");
         camParams.set(CameraParameters::KEY_SUPPORTED_SCENE_MODES, "");
